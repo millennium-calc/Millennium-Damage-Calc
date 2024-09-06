dbfa0ce54853d9582660d0a5ecacb69363f417ac
VyaxUltima <135572786+VyaxUltima@users.noreply.github.com>
2024-09-05 21:19:31 -0500

    sets

--- script_res/setdex_tt2019.js
+++ script_res/setdex_tt2019.js
@@ -1,4 +1,4 @@
-var SETDEX_TT2019 = {
+var SETDEX_TT2019 = { //todo: make sets for this shit
     //"": {
     //    "": {
     //        "level": 50,
@@ -70,6 +70,108 @@ var SETDEX_TT2019 = {
                 "Swords Dance",
                 "Zen Headbutt",
                 "Ice Punch"
+            ]
+        },
+    },
+    "Starmie": {
+        "Artemis (Mandala Kingpin)": {
+            "level": 61,
+            "evs": {
+                "hp": 8,
+                "at": 0,
+                "df": 0,
+                "sa": 252,
+                "sd": 0,
+                "sp": 248
+            },
+            "ivs": {
+                "at": 0
+            },
+            "nature": "Timid",
+            "ability": "Illuminate",
+            "item": "Expert Belt",
+            "moves": [
+                "Hydro Pump",
+                "Blizzard",
+                "Thunder",
+                "Recover"
+            ]
+        },
+    },
+    "Vulpix": {
+        "Vulpix (Gym Leader Farfa)": {
+            "level": 16,
+            "evs": {
+                "hp": 0,
+                "at": 0,
+                "df": 0,
+                "sa": 0,
+                "sd": 0,
+                "sp": 208
+            },
+            "ivs": {
+                "at": 0
+            },
+            "nature": "Timid",
+            "ability": "Flash Fire",
+            "item": "Heat Rock",
+            "moves": [
+                "Ember",
+                "Sunny Day",
+                "Disable",
+                ""
+            ]
+        },
+    },
+    "Houndour": {
+        "Houndour (Gym Leader Farfa)": {
+            "level": 15,
+            "evs": {
+                "hp": 0,
+                "at": 0,
+                "df": 0,
+                "sa": 0,
+                "sd": 0,
+                "sp": 104
+            },
+            "nature": "Naive",
+            "ability": "Flash Fire",
+            "item": "White Herb",
+            "moves": [
+                "Ember",
+                "Feint Attack",
+                "Taunt",
+                "Howl"
+            ]
+        },
+    },
+    "Arcanine": {
+        "\"Vulpix\" (Gym Leader Farfa)": {
+            "level": 15,
+            "evs": {
+                "hp": 0,
+                "at": 0,
+                "df": 0,
+                "sa": 0,
+                "sd": 0,
+                "sp": 100
+            },
+            "ivs": {
+                "hp": 10,
+                "at": 10,
+                "df": 10,
+                "sa": 10,
+                "sd": 10,
+                "sp": 10,
+            },
+            "nature": "Naive",
+            "ability": "Intimidate",
+            "item": "Oran Berry",
+            "moves": [
+                "Ember",
+                "Assurance",
+                "",
+                ""
             ]
         },
     },
